8333dd7cf07aa5c28b57420f50efabfe31dd99d1
Himangshu Saikia <saikia@kth.se>
2018-04-18 15:15:24 +0200

    Added collision avoidance

--- SocialWalker/SocialWalkerAgent.cs
+++ SocialWalker/SocialWalkerAgent.cs
@@ -3,136 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SocialWalkerAgent : Agent {
+    public int sw_id;
+    public SocialWalker agent_;
+    public GameObject target_;
+    private SocialWalkerCrowd crowd_;
 
-    public GameObject target;
-
-    private Vector3 startPos;
-    private Vector3 forward;
-    private float forwardSpeed;
-    private float rotSpeed;
-    private float forwardAcc;
-    private float rotAcc;
-
-    private float maxForwardSpeed = 0.5f;
-    private float minForwardSpeed = -0.2f;
-    private float maxRotSpeed = 90.0f;
-
-    private int history = 2;
+    private Vector3 minBound = new Vector3(-10f, 0f, -10f);
+    private Vector3 maxBound = new Vector3(10f, 0f, 10f);
 
     private int step;
-    private int episode;
-    private int numEpisodeToRepeat = 10;
 
-    private float bound = 10f;
+    void Awake(){
 
-    private List<Vector3> pastPositions;
-    private List<Vector3> pastForwardDirections;
-
-    float DistXZ(Vector3 a, Vector3 b)
-    {
-        float X = a.x - b.x;
-        float Z = a.z - b.z;
-        return Mathf.Sqrt(X * X + Z * Z);
-    }
-
-    float DistGained()
-    {
-        float sumDistGained = 0f;
-        float distToTarget = DistXZ(target.transform.position, pastPositions[0]);
-
-        for (int i = 1; i < pastPositions.Count; i++)
-        {
-            float distToTargetTmp = DistXZ(target.transform.position, pastPositions[i]);
-            sumDistGained += (distToTarget - distToTargetTmp);
-        }
-        return sumDistGained / (pastPositions.Count - 1);
     }
 
     void Start()
     {
-        Debug.Log("Input feature size " + (4 * history + 8));
         step = 0;
-        episode = 0;
-        //startPos = new Vector3(0f, 0f, 0f);
-        startPos = transform.position;
-        forward = new Vector3(0f, 0f, 1f);
-        forwardSpeed = 0f;
-        forwardAcc = 0f;
-        rotSpeed = 0f;
-        rotAcc = 0f;
-
-        pastPositions = new List<Vector3>();
-        pastForwardDirections = new List<Vector3>();
-
-        for(int i = 0; i < history; i++)
-        {
-            pastPositions.Add(startPos);
-            pastForwardDirections.Add(forward);
-        }
-
-        gameObject.transform.position = startPos;
-        gameObject.transform.rotation = Quaternion.identity;
-
-        target.transform.position = new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1));
-        //target.transform.position = new Vector3(bound - 1, 0.5f, bound - 1);
+        var go = GameObject.Find("Crowd");
+        crowd_ = go.GetComponent<SocialWalkerCrowd>();
     }
 
     public override void CollectObservations()
     {
         // Agent only remains on the 2D XZ plane
-        AddVectorObs(target.transform.position.x);
-        AddVectorObs(target.transform.position.z);
+        AddVectorObs(agent_.target.x);
+        AddVectorObs(agent_.target.z);
 
-        //add past positions
-        for(int i = 0; i < history; i++)
-        {
-            AddVectorObs(pastPositions[i].x);
-            AddVectorObs(pastPositions[i].z);
+        AddVectorObs(agent_.pos.x);
+		AddVectorObs(agent_.pos.z);
+
+        AddVectorObs(agent_.vel.x);
+		AddVectorObs(agent_.vel.z);
+
+        for(int i = 0; i < crowd_.numAgents_; i++){
+            if(i == sw_id){
+                continue;
+            }
+            AddVectorObs(crowd_.getAgent(i).pos.x);
+    		AddVectorObs(crowd_.getAgent(i).pos.z);
+            AddVectorObs(crowd_.getAgent(i).vel.x);
+    		AddVectorObs(crowd_.getAgent(i).vel.z);
         }
-
-        AddVectorObs(gameObject.transform.position.x);
-        AddVectorObs(gameObject.transform.position.z);
-
-        for (int i = 0; i < history; i++)
-        {
-            AddVectorObs(pastForwardDirections[i].x);
-            AddVectorObs(pastForwardDirections[i].z);
-        }
-
-        AddVectorObs(forward.x);
-        AddVectorObs(forward.z);
-
-        AddVectorObs(forwardSpeed);
-        AddVectorObs(rotSpeed);
     }
 
     public override void AgentReset()
     {
         step = 0;
-        forwardSpeed = 0f;
-        forwardAcc = 0f;
-        rotSpeed = 0f;
-        rotAcc = 0f;
-
-        pastForwardDirections.Clear();
-        pastPositions.Clear();
-
-        forward = new Vector3(0f, 0f, 1f);
-
-        for (int i = 0; i < history; i++)
-        {
-            pastPositions.Add(startPos);
-            pastForwardDirections.Add(forward);
-        }
-
-        gameObject.transform.position = startPos;
-        gameObject.transform.rotation = Quaternion.identity;
-        
-        if(episode > numEpisodeToRepeat)
-        {
-            episode = 0;
-            target.transform.position = new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1));
-        } 
+        Vector3 pos = new Vector3(Random.Range(minBound.x, maxBound.x), 0.5f, Random.Range(minBound.z, maxBound.z));
+        Vector3 tar = new Vector3(Random.Range(minBound.x, maxBound.x), 0.5f, Random.Range(minBound.z, maxBound.z));
+        target_.transform.position = tar;
+        agent_.init(pos, tar);
     }
 
     public override void AgentAction(float[] act, string textAction)
@@ -144,13 +65,11 @@ public class SocialWalkerAgent : Agent {
         // 2 -> turn right
         if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
         {
-            gameObject.transform.position += forward.normalized * Mathf.Clamp(act[0], 0f, 1f);
+            gameObject.transform.position += gameObject.transform.forward.normalized * Mathf.Clamp(act[0], 0f, 1f);
 
             float rotAngle = 90.0f * Mathf.Clamp(act[1], -1f, 1f); // opposite ends, reachable from [-90, 90] rotation 
 
             gameObject.transform.Rotate(0f, rotAngle, 0f);
-            forward = Quaternion.AngleAxis(rotAngle, Vector3.up) * forward;
-
         }
         else
         {
@@ -158,94 +77,45 @@ public class SocialWalkerAgent : Agent {
             //Debug.Log(action);
             if (action == 0) // accelerate forward
             {
-                forwardAcc += 0.01f;
+                agent_.accelerateForward(0.001f);
             }
             if (action == 1) // accelerate backward
             {
-                forwardAcc -= 0.01f;
+                agent_.accelerateForward(-0.005f);
             }
-            else if (action == 2) // accelerate rotation right
+            else if (action == 2) // accelerate right
             {
-                rotAcc += 0.01f;
+                agent_.accelerateRight(0.001f);
             }
-            else if (action == 3) // accelerate rotation left
+            else if (action == 3) // accelerate left
             {
-                rotAcc -= 0.01f;
+                agent_.accelerateRight(-0.001f);
             }
             else if (action == 4)
             {
-                // do nothing
-            }
-            if (action == 0 || action == 1)
-            {
-                forwardSpeed += forwardAcc;
-            }
-            if (action == 2 || action == 3)
-            {
-                rotSpeed += rotAcc;
+                agent_.maintainSpeed();
             }
 
-            forwardSpeed = Mathf.Clamp(forwardSpeed, minForwardSpeed, maxForwardSpeed);
-            rotSpeed = Mathf.Clamp(rotSpeed, -maxRotSpeed, maxRotSpeed);
+            gameObject.transform.position = agent_.pos;
+            gameObject.transform.forward = agent_.forward;
 
-            gameObject.transform.position += forward.normalized * forwardSpeed;
-            gameObject.transform.Rotate(Vector3.up, rotSpeed);
-            forward = Quaternion.AngleAxis(rotSpeed, Vector3.up) * forward;
         }
 
-        pastForwardDirections.RemoveAt(0);
-        pastForwardDirections.Add(forward);
-        pastPositions.RemoveAt(0);
-        pastPositions.Add(gameObject.transform.position);
-
-        float A = gameObject.transform.position.x - target.transform.position.x;
-        float B = gameObject.transform.position.z - target.transform.position.z;
-        float distCurr = Mathf.Sqrt(A*A + B*B);
-        float distGained = DistGained();
-
-        //Debug.Log("Average Dist Gained " + distGained);
-
-        if (distCurr < 1.0f)
+        if (agent_.targetReached())
         {
             Debug.Log("Reached Target!");
             AddReward(1.0f);
-            episode++;
             Done();
         }
-        else if (gameObject.transform.position.x < -bound || gameObject.transform.position.x > bound ||
-            gameObject.transform.position.z < -bound || gameObject.transform.position.z > bound)
+        else if (!agent_.withinBounds(minBound, maxBound))
         {
             //Debug.Log("Went out of Arena!");
             AddReward(-0.5f);
-            episode++;
-            Done();
-
-            //Vector3 tmp = gameObject.transform.position;
-
-            //if (tmp.x < -bound)
-            //{
-            //    tmp.x = -bound;
-            //}
-            //if (tmp.z < -bound)
-            //{   
-            //    tmp.z = -bound;
-            //}   
-            //if (tmp.x > bound)
-            //{   
-            //    tmp.x = bound;
-            //}   
-            //if (tmp.z > bound)
-            //{   
-            //    tmp.z = bound;
-            //}
-
-            //gameObject.transform.position = tmp;
+            Done();    
+        } else if(crowd_.doesCollide(sw_id)){
+            AddReward(-0.5f);
         }
-        else if (distGained > 0)
-        {
-            AddReward(distGained);
-        }
-
+                
         AddReward(-0.01f);
         
     }
--- SocialWalker/SocialWalkerCrowd.cs
+++ SocialWalker/SocialWalkerCrowd.cs
@@ -4,15 +4,97 @@ using UnityEngine;
 
 public struct SocialWalker
 {
-    public Vector3 x;
-    public Vector3 v;
-    public Vector3 a;
+    public Vector3 pos;
+    public Vector3 vel;
+    public Vector3 acc;
+    public Vector3 forward;
+    public Vector3 target;
+    public float radius;
 
-    public void init(Vector3 x_, Vector3 v_, Vector3 a_)
+    float maxSpeed;
+    float minSpeed;
+
+    public void init(Vector3 x_, Vector3 target_)
     {
-        x = x_;
-        v = v_;
-        a = a_;
+        pos = x_;
+        vel = new Vector3(0f, 0f, 0f);
+        acc = new Vector3(0f, 0f, 0f);
+        forward = new Vector3(0f, 0f, 1f);
+        target = target_;
+        radius = 1.0f;
+        maxSpeed = 0.5f;
+        minSpeed = 0.001f;
+    }
+
+    private void restrictSpeed(){
+        if(vel.magnitude > maxSpeed){
+            vel = vel.normalized * maxSpeed;
+        }
+        if(vel.magnitude < minSpeed){
+            vel = vel.normalized * minSpeed;
+        }
+    }
+
+    // accelerate in the direction of forward velocity
+    public void accelerateForward(float inc){ 
+        //Debug.Log("Accelerating Forward");
+        acc = acc + forward * inc;
+        vel = vel + acc; // unit time
+
+        restrictSpeed();
+
+        pos = pos + vel; // unit time
+        if(vel.magnitude > 0.0001f){
+            forward = vel.normalized;
+        }
+    }
+
+    public void accelerateRight(float inc){
+        // Since our plane is the XZ plane, we take the cross product of the forward direction with the up direction (0, 1, 0)
+        // to get the right direction 
+        Vector3 right = Vector3.Cross(new Vector3(0f, 1f, 0f), forward).normalized;
+        acc = acc + right * inc;
+        vel = vel + acc;
+
+        restrictSpeed();
+
+        pos = pos + vel;
+        if(vel.magnitude > 0.0001f){
+            forward = vel.normalized;
+        }
+    }
+
+    public void maintainSpeed(){
+        acc = new Vector3(0f, 0f, 0f);
+        pos = pos + vel;
+        if(vel.magnitude > 0.0001f){
+            forward = vel.normalized;
+        }
+    }
+
+    public bool targetReached(){
+        Vector3 d = target - pos;
+        if(d.magnitude < 1.0f){
+            return true;
+        }
+        return false;
+    }
+
+    public bool withinBounds(Vector3 minB, Vector3 maxB){
+        if(pos.x < minB.x || pos.x > maxB.x || pos.z < minB.z || pos.z > maxB.z){
+            return false;
+        }
+        return true;
+    }
+
+    public bool isCollidingWith(SocialWalker sw)
+    {
+        Vector3 d = pos - sw.pos;
+        if(radius + sw.radius > d.magnitude)
+        {
+            return true;
+        }
+        return false;
     }
 }
 
@@ -28,7 +110,7 @@ public class SocialWalkerCrowd : MonoBehaviour
 
     void Start()
     {
-        
+        agents_ = new List<SocialWalker>();
     }
 
     void Update()
@@ -43,23 +125,50 @@ public class SocialWalkerCrowd : MonoBehaviour
 
     void ResetAgents()
     {
+        agents_.Clear();
         for(int i = 0; i < numAgents_; i++)
         {
             float hue = (float)i / numAgents_;
             Color newColor = Color.HSVToRGB(hue, 1f, 1f);
             Color newColorDark = Color.HSVToRGB(hue, 1f, 0.5f);
-            
-            GameObject walkerClone = Instantiate(walkerPrefab_, new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1)), Quaternion.identity);
+
+            Vector3 pos = new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1));
+            Vector3 tar = new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1));
+
+            SocialWalker S = new SocialWalker();
+            S.init(pos, tar);
+            agents_.Add(S);
+
+            GameObject walkerClone = Instantiate(walkerPrefab_, pos , Quaternion.identity);
             walkerClone.GetComponent<MeshRenderer>().material.color = newColor;
 
-            GameObject targetClone = Instantiate(targetPrefab_, new Vector3(Random.Range(1 - bound, bound - 1), 0.5f, Random.Range(1 - bound, bound - 1)), Quaternion.identity);
+            GameObject targetClone = Instantiate(targetPrefab_, tar, Quaternion.identity);
             targetClone.GetComponent<MeshRenderer>().material.color = newColorDark;
 
             //Brain brainClone = Instantiate(socialWalkerBrain_.GetComponent<Brain>());
             //walkerClone.GetComponent<SocialWalkerAgent>().brain = brainClone;
-            walkerClone.GetComponent<SocialWalkerAgent>().brain = socialWalkerBrain_.GetComponent<Brain>();
-            walkerClone.GetComponent<SocialWalkerAgent>().target = targetClone;
+            walkerClone.GetComponent<SocialWalkerAgent>().GiveBrain(socialWalkerBrain_.GetComponent<Brain>());
+            walkerClone.GetComponent<SocialWalkerAgent>().agent_ = S;
+            walkerClone.GetComponent<SocialWalkerAgent>().target_ = targetClone;
+            walkerClone.GetComponent<SocialWalkerAgent>().sw_id = i;
         }
+    }
+
+    public SocialWalker getAgent(int id)
+    {
+        return agents_[id];
+    }
+
+    public bool doesCollide(int id){
+        for(int i = 0; i < numAgents_; i++){
+            if(i == id){
+                continue;
+            }
+            if(agents_[id].isCollidingWith(agents_[i])){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
